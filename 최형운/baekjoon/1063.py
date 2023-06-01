be0c9282955bc8dd8221f34fe2104a804ee5e40f
choihyoingwoon <cpobo@hanmail.net>
2023-06-01 16:01:26 +0900

    upload

--- 최형운/baekjoon/1063.py
+++ 최형운/baekjoon/1063.py
@@ -1,16 +1,20 @@
-import sys
-input=sys.stdin.readline
-move={'R':[1,0],"L":[-1,0],'B':[0,-1],'T':[0,1],'RT':[1,1],"L":[-1,1],'RB':[1,-1],'LB':[-1,-1]}
-k,dol,n=input().split()
-kPosi=[ord(k[0]) - 64,int(k[1])]
-dolPosi=[ord(k[0]) - 64,int(dol[1])]
-for _ in range(int(n)):
-    moving=input()
-    a=move[moving[:-1]]
-    if 1<=kPosi[0]+a[0]<=8 and 1<=kPosi[1]+a[1]<=8:
-        kPosi = [kPosi[0]+a[0], kPosi[1]+a[1]]
-    if 1<=dolPosi[0]+a[0]<=8 and 1<=dolPosi[1]+a[1]<=8:
-        dolPosi = [dolPosi[0]+a[0], dolPosi[1]+a[1]]
+king, stone, N = input().split()
+k = list(map(int, [ord(king[0]) - 64, king[1]]))
+s = list(map(int, [ord(stone[0]) - 64, stone[1]]))
+move = {'R': [1, 0], 'L': [-1, 0], 'B': [0, -1], 'T': [0, 1], 'RT': [1, 1], 'LT': [-1, 1], 'RB': [1, -1], 'LB': [-1, -1]}
 
-print(chr(kPosi[0] + 64)+str(kPosi[1]))
-print(chr(dolPosi[0] + 64)+str(dolPosi[1]))
+for _ in range(int(N)):
+    m = input()
+    nx = k[0] + move[m][0]
+    ny = k[1] + move[m][1]
+    if 0 < nx <= 8 and 0 < ny <= 8:
+        if nx == s[0] and ny == s[1]:
+            sx = s[0] + move[m][0]
+            sy = s[1] + move[m][1]
+            if 0 < sx <= 8 and 0 < sy <= 8:
+                k = [nx, ny]
+                s = [sx, sy]
+        else:
+            k = [nx, ny]
+print(f'{chr(k[0] + 64)}{k[1]}')
+print(f'{chr(s[0] + 64)}{s[1]}')
