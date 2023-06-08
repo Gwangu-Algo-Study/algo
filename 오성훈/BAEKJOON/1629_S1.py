32f5354359e0b82022c8c2d4c32228c825b9f615
OctoHun <97264011+OctoHun@users.noreply.github.com>
2023-06-08 23:35:03 +0900

    백준 1629 S1

---
+++ 오성훈/BAEKJOON/1629_S1.py
@@ -0,0 +1,13 @@
+def cal(a, b, c):
+    if b == 1:
+         return a % c
+    
+    temp = cal(a, b//2, c)
+
+    if b % 2 == 1:
+         return temp * temp * a % c
+    else:
+         return temp * temp % c
+
+a, b, c = map(int, input().split())
+print(cal(a, b, c))
