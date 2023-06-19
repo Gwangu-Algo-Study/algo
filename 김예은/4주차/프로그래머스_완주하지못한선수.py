d2cb13c08e2554291091826ece725effc06b8f59
yeni28 <suntofu0228@naver.com>
2023-06-20 00:01:14 +0900

    0619 add1 수정

--- 김예은/4주차/프로그래머스_완주하지못한선수.py
+++ 김예은/4주차/프로그래머스_완주하지못한선수.py
@@ -1,16 +1,23 @@
-
-par
 def solution(participant, completion):
     runners = {}
     # 참가자의 명단을 딕셔너리에 추가해준다.
     for i in participant:
-        runners[i] += 1
+        if i in runners:
+            # 참가자가 있다면 1을 더해준다.
+            runners[i] += 1
+        else:  # 참가자가 없다면 기본 값인 1이다
+            runners[i] = 1
+
+    for j in completion:
+        if j in runners:
+            runners[j] -= 1
+        else:
+            continue
 
     for key, value in runners.items():
-        for j in completion:
-            if key == j:
-                runners[key] -= 1
-        if value == 0:
-            answer = value
+        # 만약 value값이 0이 아니라면, -> completion에 없다면
+        if value != 0:
+            #         그 범인이 바로 미완주자입니다.
+            answer = str(key)
 
     return answer
